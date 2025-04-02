27e055479b3ae9b4a372bb11502bde739ec36096
Roman Leshin <vorzee@gmail.com>
2025-04-02 18:24:01 +0500

    task 1

--- cypress.config.js
+++ cypress.config.js
@@ -6,7 +6,7 @@ module.exports = defineConfig({
         setupNodeEvents(on, config) {
             // implement node event listeners here
         },
-        retries: 2,
+        retries: 0,
         baseUrl: "http://localhost:3000",
     },
 });
--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,4 +1,4 @@
-describe("Тестирование приложения для хранения и скачивания книг", () => {
+describe("Лекционные тесты", () => {
     // Первый лекционный тест
     it("Should successfully login", () => {
         cy.visit("/");
@@ -20,5 +20,63 @@ describe("Тестирование приложения для хранения 
         cy.get("#pass")
             .then(($el) => $el[0].checkValidity())
             .should("be.false");
+    });
+});
+
+it.skip("Добавление книг в библиотеку", () => {
+    const classicBooks = [
+        {
+            title: "Преступление и наказание",
+            authors: "Фёдор Достоевский",
+        },
+        {
+            title: "Война и мир",
+            authors: "Лев Толстой",
+        },
+        {
+            title: "Мастер и Маргарита",
+            authors: "Михаил Булгаков",
+        },
+    ];
+
+    cy.visit("/");
+    cy.login("test@test.com", "test");
+    classicBooks.forEach(({ title, authors }) => cy.addBook(title, authors));
+});
+
+describe.only("Домашнее задание", () => {
+    beforeEach(() => {
+        cy.visit("/");
+        cy.login("test@test.com", "test");
+        cy.clearFavorites();
+        cy.visit("/");
+    });
+    afterEach(() => {
+        cy.logout();
+    });
+
+    it("Добавление книги в избранное", () => {
+        const title = "Война и мир";
+        cy.getCardByTitle(title).contains("Add to favorite").click();
+        cy.visit("/favorites");
+        cy.getCardByTitle(title)
+            .contains("Delete from favorite")
+            .should("have.text", "Delete from favorite");
+    });
+
+    it("Удаление книги из избранного", () => {
+        const title = "Война и мир";
+        cy.getCardByTitle(title).contains("Add to favorite").click();
+        cy.visit("/favorites");
+        cy.getCardByTitle(title).contains("Delete from favorite").click();
+        cy.visit("/");
+        cy.getCardByTitle(title)
+            .contains("Add to favorite") // Кнопка снова доступна
+            .should("have.text", "Add to favorite");
+    });
+
+    it("Проверка текста по эндпоинту при пустом избранном", () => {
+        cy.visit("/favorites");
+        cy.contains("Please add some book to favorit on home page!");
     });
 });
